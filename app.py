0d6bbbf3e63c3b8dc9ffd843a55169c58c4a04c4
Nadav Massad <massadnadav@gmail.com>
2023-01-16 13:04:00 +0200

    message updates

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SECRET_KEY'] = "random string"
- 
+
 db = SQLAlchemy(app)
 
+
 class Phones(db.Model):
-    id = db.Column('animal_id', db.Integer, primary_key = True)
+    id = db.Column('animal_id', db.Integer, primary_key=True)
     brand = db.Column(db.String(100))
     color = db.Column(db.String(50))
     price = db.Column(db.Integer)
@@ -22,43 +23,47 @@ class Phones(db.Model):
         self.price = price
 
 
-@app.route('/phones/<id>', methods = ['GET', 'POST', 'DELETE', 'PUT'])
-@app.route('/phones/', methods = ['GET', 'POST', 'DELETE', 'PUT'])
-def crud_phone(id=id):
+@app.route('/phones/<id>', methods=['GET', 'POST', 'DELETE', 'PUT'])
+@app.route('/phones/', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def crud_phone(id=-1):
     if request.method == 'GET':
         res = []
         for phone in Phones.query.all():
-            res.append({'id': phone.id, 'brand': phone.brand, 'color': phone.color, 'price': phone.price})
+            res.append({'id': phone.id, 'brand': phone.brand,
+                       'color': phone.color, 'price': phone.price})
         return (json.dumps(res))
     if request.method == 'POST':
         request_data = request.get_json()
         brand = request_data['brand']
         color = request_data['color']
         price = request_data['price']
-        new_car = Phones(brand, color, price)
-        db.session.add (new_car)
+        new_phone = Phones(brand, color, price)
+        db.session.add(new_phone)
         db.session.commit()
         return 'New Phone Was Added'
     if request.method == 'DELETE':
-        del_car = Phones.query.get(id)
-        db.session.delete(del_car)
+        del_phone = Phones.query.get(id)
+        db.session.delete(del_phone)
         db.session.commit()
-        return {"msg": 'A Phone Was Deleted'}
+        return 'A Phone Was Deleted'
     if request.method == 'PUT':
-        update_car = Phones.query.get(id)
+        update_phone = Phones.query.get(id)
         brand = request.json['brand']
         color = request.json['color']
         price = request.json['price']
-        update_car.brand = brand
-        update_car.color = color
-        update_car.price = price
+        update_phone.brand = brand
+        update_phone.color = color
+        update_phone.price = price
         db.session.commit()
-        return  {"msg: 'A Phone Was Edited'"}
+        return 'A Phone Was Edited'
+
 
 @app.route('/')
 def test():
     return 'Test'
- 
+
+
 if __name__ == '__main__':
-    with app.app_context():db.create_all()
-    app.run(debug = True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
